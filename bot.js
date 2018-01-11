796539a65cd989bb1115405c653a5d6d87e75ddf
pablackhawk <l.tirtarahardja@gmail.com>
2018-01-11 00:34:48 -0800

    Modified auth.json to have config settings, refined commands

--- bot.js
+++ bot.js
@@ -1,37 +1,31 @@
 const Discord = require('discord.js')
-const logger = require('winston')
-const auth = require('./auth.json')
-const token = auth.token
+const fs = require('fs')
+const config = require('./auth.json')
+const token = config.token
 let bridge = '#393944416282214402'
 let crewActivity = '#393944470485336064'
 let ooc = '#393944502009593879'
-// logger settings configuration
-logger.remove(logger.transports.Console)
-logger.add(logger.transports.Console, {
-  colorize: true,
-})
-logger.level = 'debug'
 
 // Bot initialization
-let bot = new Discord.Client()
-bot.on('ready', function(evt) {
-  logger.info('Connected')
-  logger.info('Logged in')
+const bot = new Discord.Client()
+bot.on('ready', () => {
+  console.log('Bot logged in and ready')
 })
 bot.login(token)
-bot.on('message', message => {
-  switch (message.content) {
-    // !ping
-    case '!ping':
-      message.reply('Pong!')
+// Bot replies
+bot.on('message', msg => {
+let args = msg.content.slice(config.prefix.length).trim().split(/ + /g)
+let command = args.shift().toLowerCase()
+  if(!command || msg.author.bot)return
+  switch (command) {
+    case 'ping':
+      msg.reply('Pong!')
       break
-    // !hello
-    case '!hello':
-      message.reply('Hello! Welcome to the server!')
+    case 'hello':
+      msg.reply('Hello! Welcome to the server!')
       break
-    // !spoilers
-    case '!spoilers':
-      message.reply(
+    case 'spoilers':
+      msg.reply(
         'Absolutely NO spoilers for the latest episode in general channels until the Wednesday after it airs. All spoiler-related discussions should stay in <' +
           bridge +
           '>.\n\nSpoilers are banned in <' +
@@ -41,23 +35,20 @@ bot.on('message', message => {
           '> for the first 24 hours after airing.'
       )
       break
-    // !sot
-    case '!sot':
-      message.reply(
+    case 'sot':
+      msg.reply(
         'Shield of Tomorrow currently airs Fridays at 4PM Pacific Time (GMT-8) on the Geek and Sundry Twitch channel (https://www.twitch.tv/geekandsundry)'
       )
       break
-    // !talkabout
-    case '!talkabout':
-      message.reply("I don't know what you're talking about...")
+    case 'talkabout':
+      msg.reply("I don't know what you're talking about...")
       break
-    // !cato
-    case '!cato':
-      message.reply('Cato: SHIP! COMBAT!')
+    case 'cato':
+      msg.reply('Cato: SHIP! COMBAT!')
       break
-    // !belowdeckspodcast
-    case '!belowdeckspodcast':
-      message('Follow the Below Decks Podcast! https://www.belowdeckspodcast.com')
+    case 'belowdeckspodcast':
+      msg.reply('Follow the Below Decks Podcast! https://www.belowdeckspodcast.com')
       break
   }
 })
+ 
